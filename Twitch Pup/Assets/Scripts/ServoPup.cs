edc02487b983916a01fd7c5abe886d5538d1ba7c
mariamjasminenaziripour <mnaziripour@tbsf5ksf0daf9vn.turner.com>
2017-04-29 15:00:26 -0400

    Moved In

---
+++ Twitch Pup/Assets/Scripts/ServoPup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniduino;
+
+public class ServoPup : MonoBehaviour {
+
+	public Arduino arduino;
+
+	public int pin = 9;
+
+	// Use this for initialization
+	void Start () {
+		arduino = Arduino.global;
+		arduino.Setup (ConfigurePins);
+
+		StartCoroutine (pupLoop ());
+	}
+
+	void ConfigurePins(){
+		arduino.pinMode (pin, PinMode.SERVO);
+	}
+	
+	IEnumerator pupLoop()
+	{
+		while(true)
+		{                  
+			arduino.analogWrite(pin, 0);
+			yield return new WaitForSeconds(1);
+
+			arduino.analogWrite(pin, 180);
+			yield return new WaitForSeconds(1);                
+		}
+
+	}
+
+
+}
---
+++ Twitch Pup/Assets/Scripts/ServoPupControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniduino;
+
+public class ServoPupControl : MonoBehaviour {
+
+	public Arduino arduino;
+	public int servo_pin = 9;
+	private int servo_pos = 90;
+
+	// Use this for initialization
+	void Start () {
+		arduino = Arduino.global;
+		arduino.Log = (s) => Debug.Log("Arduino: " +s);
+
+		arduino.Setup(ConfigurePins);
+
+
+		ConfigurePins ();
+	}
+
+	void ConfigurePins ()
+	{
+		Debug.Log("set pin mode");
+		arduino.pinMode(servo_pin, PinMode.SERVO);
+	}
+
+	void servoLeft(){
+
+	}
+
+	void servoRight(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
